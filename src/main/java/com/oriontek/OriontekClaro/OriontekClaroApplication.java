bc502a984e0abbdd94e61aa9ec2ed003a8e17cb6
Jean Carlos George <jean18699@gmail.com>
2025-05-13 16:56:01 -0400

    Initial commit
    Project and initial folders created

---
+++ src/main/java/com/oriontek/OriontekClaro/OriontekClaroApplication.java
@@ -0,0 +1,13 @@
+package com.oriontek.OriontekClaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriontekClaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriontekClaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oriontek/OriontekClaro/OriontekClaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oriontek.OriontekClaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriontekClaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
